4d08e142bd365241ef459501968659403c2916fd
ASHISH VERMA <ashishkv845@gmail.com>
2024-01-23 18:10:42 +0530

    add new feature

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const nav = document.getElementById('navbar')
+const nav = document.getElementById("navbar");
 
-window.addEventListener('scroll', ()=>{
-    if (window.scrollY > 2){
-        nav.classList.add('navSticky')
-    }
-    else{
-        nav.classList.remove('navSticky')
-        nav.style.scrollBehavior = 'smooth'
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 2) {
+    nav.classList.add("navSticky");
+  } else {
+    nav.classList.remove("navSticky");
+    nav.style.scrollBehavior = "smooth";
+  }
+});
 
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
     
-    $('.owl-carousel').owlCarousel({
-        margin:10,
-        loop:true,
-        autoWidth:true,
-        items:1
-    })
+    autoplayHoverPause: true,
   });
-  
+});
